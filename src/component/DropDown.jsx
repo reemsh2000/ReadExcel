8fb5e36ff27c538be9e2c408b8220bdf0e684618
reemsh2000 <sh951652@gmail.com>
2021-12-13 17:52:54 +0200

    edits

--- src/component/DropDown.jsx
+++ src/component/DropDown.jsx
@@ -1,27 +1,35 @@
-import "./style/dropDownStyle.css";
-import { useState } from "react";
-import Submit from "./submit";
+import './style/dropDownStyle.css'
+import { useState } from 'react'
+import Submit from './submit'
 function DropDown({ columns }) {
-  const [selectedColumn, setSelectedColumn] = useState(columns[2]);
+  const [selectedColumn, setSelectedColumn] = useState('')
 
   const handleChange = (e) => {
-    setSelectedColumn(e.target.value);
-  };
+    setSelectedColumn(e.target.value)
+  }
   return (
-    <div className='drop-down-section'>
-    <p> Choose the Column name that Contain the text</p>
-      <select name="columnNames" onChange={handleChange} className="drop-down"value={selectedColumn} >
+    <div className="drop-down-section">
+      <p> Choose the Column name that Contain the text</p>
+      <select
+        name="columnNames"
+        onChange={handleChange}
+        className="drop-down"
+         value={selectedColumn}
+      >
+      
         {columns.map((columnName, index) => {
-          return (
+          return columnName !=='' && columnName !==null ? (
             <option key={index} value={columnName}>
               {columnName}
             </option>
-          );
+          ) : (
+            ''
+          )
         })}
       </select>
       {selectedColumn && <Submit />}
     </div>
-  );
+  )
 }
 
-export default DropDown;
+export default DropDown
--- src/component/submit.jsx
+++ src/component/submit.jsx
@@ -1,9 +1,14 @@
 import "./style/submitSectionStyle.css";
+import {useRef} from 'react'
 function Submit() {
+  const congratsMessage = useRef(null)
+  const submitHandle =()=>{
+    congratsMessage.current.style.display='block';
+  }
   return (
     <div className="submit-section">
-      <p>All is Done !!</p>
-      <button className="submit-button">Submit</button>
+      <p ref={congratsMessage}>All is Done !!</p>
+      <button className="submit-button"  onClick={submitHandle}>Submit</button>
     </div>
   );
 }
